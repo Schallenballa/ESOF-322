7a53b606b64f81c699acb9cb59a11c7e1f83a114
Zachary Schallenberger <zapps98@gmail.com>
2019-10-09 22:28:31 -0600

    Lab 1 Commit
    This is a commit for all of Lab 1

---
+++ Lab 1/src/Company.java
@@ -0,0 +1,46 @@
+
+
+public class Company { private String name;}
+//This first lab was entirely pseudocode to compliment the UML diagrams for Lab 1
+/*
+private Address headquarters;
+}
+public class Truck extends Vehicle {
+private UnlimitedNatural weight; private UnlimitedNatural power; private String manufacturer; private String RegNo;
+}
+public class Car extends Vehicle {
+private CarKind kind;
+private UnlimitedNatural noSeats; private UnlimitedNatural power; private String manufacturer; private String RegNo;
+}
+public class Motorbike extends Vehicle {
+private MbKind kind;
+private UnlimitedNatural cylinderCap; private UnlimitedNatural power; private String manufacturer;
+private String RegNo;
+}
+public abstract class Vehicle implements IRentable {
+}
+public interface IRentable {
+public void Rent();
+}
+public class VehicleRentalService extends Service {
+private Vehicle vehicle; public void offerCar(); public void offerMotorbike(); public void offerTruck();
+}
+public abstract class Service {
+private Customer customer;
+}
+public abstract class Person {
+private String name; private String email;
+}
+public class Employee extends Person{
+}
+public class Customer extends Person{
+}
+Tristan Stevens Zachary Schallenberger
+public class Subcontractor extends Customer, Employee {
+}
+public class Address {
+private String street; private String postalCode; private String city;
+}
+public class BankAccount {
+}
+*/
